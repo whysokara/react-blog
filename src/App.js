21bc39b3bfc12814f0e23c1e18066454d1310f35
Himanshu Kara <kara_himanshu@yahoo.com>
2021-07-08 00:09:39 +0530

    Topbar Added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Header from "./header/Header";
 import Topbar from "./topbar/Topbar";
 
 function App() {
   return (
     <div className="App">
       <Topbar />
+      <Header />
     </div>
   );
 }
