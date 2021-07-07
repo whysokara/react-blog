58c17e4a1760faff4676f343aaf928da848b0c94
Himanshu Kara <kara_himanshu@yahoo.com>
2021-07-08 00:52:14 +0530

    Home Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import Header from "./header/Header";
+import Home from "./pages/home/Home";
 import Topbar from "./topbar/Topbar";
 
 function App() {
   return (
     <div className="App">
       <Topbar />
-      <Header />
+      <Home />
     </div>
   );
 }
